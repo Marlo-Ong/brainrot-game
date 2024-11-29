359d0f757e70131fbaa75e74e6e64bda31fc0768
Marlo Ongkingco <moon.ongkingco@gmail.com>
2024-11-29 13:20:12 -0800

    Add unskippable ads

---
+++ Assets/Scripts/Advertisement.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class Advertisement : MonoBehaviour
+{
+    public int auraPointsToWin;
+    public int auraPointsToLose;
+
+    public VideoClip[] clipsToPlay;
+    public RenderTexture[] correspondingTextures;
+    public GameObject playerPrefab;
+
+    [Tooltip("Minimum number of seconds before ad can play")]
+    public int frequencyMin;
+    [Tooltip("Maximum number of seconds before ad can play")]
+    public int frequencyMax;
+
+    public AudioClip OnStartSound;
+    public AudioClip OnSuccessSound;
+
+    void Start()
+    {
+        StartNewAd();
+    }
+
+    public void StartNewAd()
+    {
+        AudioManager.PlaySound(this.OnStartSound);
+
+        // Create a new ad
+        var newAdObject = Instantiate(playerPrefab);
+        newAdObject.transform.SetParent(this.transform);
+        newAdObject.transform.localPosition = Vector3.zero;
+
+        // newAdObject.transform.localPosition = new Vector3(
+        //     newAdObject.transform.localPosition.x + Random.Range(-50f, 50f),
+        //     newAdObject.transform.localPosition.x + Random.Range(-50f, 50f)
+        // );
+
+        var videoComponent = newAdObject.GetComponent<Video>();
+        videoComponent.PlaceInFront();
+
+        var imageComponent = videoComponent.gameObject.GetComponentInChildren<RawImage>();
+        var sliderComponent = videoComponent.gameObject.GetComponentInChildren<Slider>();
+        var newAd = videoComponent.gameObject.GetComponentInChildren<VideoPlayer>();
+
+        int i = Random.Range(0, clipsToPlay.Length);
+        newAd.clip = clipsToPlay[i];
+        imageComponent.texture = correspondingTextures[i];
+        newAd.targetTexture = correspondingTextures[i];
+
+        newAd.Play();
+        StartCoroutine(DestroyAd((float)newAd.length, newAdObject));
+        StartCoroutine(UpdateSlider(sliderComponent, newAd));
+    }
+
+    IEnumerator DestroyAd(float timer, GameObject ad)
+    {
+        yield return new WaitForSeconds(timer);
+        Destroy(ad);
+
+        yield return new WaitForSeconds(Random.Range(frequencyMin, frequencyMax));
+        StartNewAd();
+    }
+
+    IEnumerator UpdateSlider(Slider slider, VideoPlayer ad)
+    {
+        while (ad != null && ad.time < ad.length)
+        {
+            slider.value = (float)(ad.time / ad.length);
+            yield return null;
+        }
+    }
+
+    public virtual void Win()
+    {
+        AudioManager.PlaySound(this.OnSuccessSound);
+        GameManager.AddAuraPoints(this.auraPointsToWin);
+    }
+}
